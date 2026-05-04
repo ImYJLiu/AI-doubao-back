2e7e41d60d08f6ee8d3a2d2449cb2998976a8e97
ImYJLiu <1627465617@qq.com>
2026-05-05 00:19:54 +0800

    revert: remove NoResourceFoundException handler

--- src/main/java/com/doubao/watermark/common/GlobalExceptionHandler.java
+++ src/main/java/com/doubao/watermark/common/GlobalExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.doubao.watermark.common;
 
+package com.doubao.watermark.common;
+
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,12 +18,6 @@ public class GlobalExceptionHandler {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
         return Result.error(e.getCode(), e.getMessage());
-    }
-
-    @ExceptionHandler(NoResourceFoundException.class)
-    public Result<?> handleNoResourceFound(NoResourceFoundException e, HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        return Result.error(404, "Not Found");
     }
 
     @ExceptionHandler(Exception.class)
